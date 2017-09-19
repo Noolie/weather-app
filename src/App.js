349a041a8d56b00ef50c0ece48b7554e00ed346e
Candy <Nooolie@gmail.com>
2017-09-19 21:14:46 +0300

    1.1.0 search field added

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class SmallWeatherInfo extends React.Component {
     return(
         <li data-id={this.props.dataId} onClick={this.props.makesActive} className={'single-day ' + (this.props.dataId === this.props.isActive?'is-active-small':'')}>
           <div className='day-time'>{this.getDate(this.props.time)}</div>
-          <img className='day-weather' height='30' src={'big-icons/' + this.props.icon + '.png'} alt={this.props.icon} />
+          <img className='day-weather' height='30' src={'big-icons/' + (this.props.icon.match(/n/)?this.props.icon.replace(/n/, 'd'):this.props.icon) + '.png'} alt={this.props.icon.match(/n/)?this.props.icon.replace(/n/, 'd'):this.props.icon} />
           <div className='day-temp'>{this.props.temp} &deg;</div>
         </li>
     )
@@ -41,7 +41,7 @@ class FullWeatherInfo extends React.Component {
       <div>
         <div data-id={this.props.dataId} className={'single-day-full'}>
           <h1 className='day-time-full'>{this.getFullDate(this.props.time)}</h1>
-          <img className='day-icon-full' src={'big-icons/' + this.props.icon + '.png'} alt={this.props.icon}/>
+          <img className='day-icon-full' src={'big-icons/' + (this.props.icon.match(/n/)?this.props.icon.replace(/n/, 'd'):this.props.icon) + '.png'} alt={this.props.icon.match(/n/)?this.props.icon.replace(/n/, 'd'):this.props.icon}/>
           <p className='day-temp-full'>{this.props.temp} &deg;</p>
           <p className='day-weather-full'>{this.props.weather}</p>
         </div>
@@ -56,7 +56,7 @@ class FullWeatherInfo extends React.Component {
           <h1>humidity</h1>
         </div>
         <div className='day-country-full'>
-          <p>Ukraine, Odessa</p>
+          <p>{this.props.city}</p>
         </div>
       </div>
     )
@@ -67,43 +67,66 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      initText: 'loading...',
+      initText: 'loading...'
     }
   }
 
   componentDidMount(){
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=Odessa,ua&units=metric&appid=3ad0ec109fcb8d87875c504182f2cee5')
+    fetch('country-city-data.json')
       .then((response) => {
-          if (response.status !== 200) {
-            console.log('Status error: ' +
-              response.status);
-            return;
-          } else {
-            return response.json();
-          }
+        if (response.status !== 200) {
+          console.log('Status error: ' + response.status);
+          return;
+        } else {
+          return response.json();
         }
-      )
-      .then((data) => {
-        this.setState({
-          data: data
-        })
-        return data;
       })
-      .then((data) => {
-        let initId = data.list.filter(function(item) {
-          return item.dt_txt.match(/12:00:00/) != null
+      .then((CCData) => {
+        CCData.sort((a,b) => {
+          if(a.nm < b.nm) return -1;
+          if(a.nm > b.nm) return 1;
+          return 0;
         })
         this.setState({
-          isActiveId: initId[0].dt
+          CCData: CCData
         })
       })
-      .catch((error) => {
-        console.log('Fetch Error: ', error);
-        this.setState({
-          initText: 'Ops! looks like we caught an error'
+    }
+
+  getData = (ev) => {
+      let resultLink = 'https://api.openweathermap.org/data/2.5/forecast?id=' + ev.target.getAttribute('data-city-id') + '&units=metric&appid=3ad0ec109fcb8d87875c504182f2cee5';
+        fetch(resultLink)
+          .then((response) => {
+            if (response.status !== 200) {
+              console.log('Status error: ' +response.status);
+              return;
+            } else {
+              return response.json();
+            }
+          }
+        )
+        .then((data) => {
+          this.setState({
+            data: data,
+            CCName: ev.target.getAttribute('data-CC-name')
+          })
+          return data;
         })
-      });
-  }
+        .then((data) => {
+          let initId = data.list.filter(function(item) {
+            return item.dt_txt.match(/12:00:00/) != null
+          })
+          this.setState({
+            isActiveId: initId[0].dt
+          })
+        })
+        .catch((error) => {
+          console.log('Fetch Error: ', error);
+          this.setState({
+            initText: 'Ops! looks like we caught an error'
+          })
+        })
+      }
 
   openFullInfo(ev){
     let smallWeatherList = document.querySelectorAll('.single-day')
@@ -123,12 +146,43 @@ class App extends React.Component {
     }
   }
 
+  searchCity(ev){
+    if(ev.key !== 'Enter' && ev.target.tagName !== 'BUTTON') return;
+    let searchValue = this.refs.searchField.value;
+    if(searchValue.length < 3) return;
+    this.refs.searchOutput.innerHTML = '';
+    for(let i = 0; i < this.state.CCData.length; i++){
+      if(this.state.CCData[i].nm.toLowerCase().indexOf(searchValue.toLowerCase()) === -1) continue
+      let listItem = document.createElement('LI');
+      listItem.classList.add('search-result');
+      listItem.setAttribute('data-city-id', this.state.CCData[i].id);
+      listItem.setAttribute('data-CC-name',this.state.CCData[i].countryCode +', '+ this.state.CCData[i].nm);
+      listItem.addEventListener('click', this.getData);
+      listItem.innerHTML = this.state.CCData[i].nm + ' - ' + this.state.CCData[i].countryCode;
+      this.refs.searchOutput.appendChild(listItem)
+    }
+  }
+
+  backToStart(){
+    this.setState({
+      CCName: '',
+      data: ''
+    })
+  }
+
   render() {
+    if(!this.state.CCName) return (
+      <div className='search-board'>
+        <button className='search-button ' onClick={this.searchCity.bind(this)}>&#128269;</button>
+        <input className='search-field' ref='searchField' type='text' placeholder='city' onKeyPress={this.searchCity.bind(this)} />
+        <ul ref='searchOutput' className='search-output'></ul>
+      </div>
+    )
     if(!this.state.data) return <p className='notification'>{this.state.initText}</p>
     return (
       <div className='weather-board'>
+        <button className='return-button' onClick={this.backToStart.bind(this)}>return</button>
         <ul className="small-weather-info">
-
           {this.state.data.list.map((item,i) => {
               if(!item.dt_txt.match(/12:00:00/)) return;
               return (
@@ -163,6 +217,7 @@ class App extends React.Component {
                   icon={item.weather[0].icon}
                   pressure={Math.round(item.main.grnd_level)}
                   humidity={item.main.humidity}
+                  city={this.state.CCName}
                 />
               )
             }
